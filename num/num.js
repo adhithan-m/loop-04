ec7a1654fb0786e792e5aa86477e1d571a477834
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 16:23:50 +0530

    num.

---
+++ num/num.js
@@ -0,0 +1,29 @@
+//var a = parseInt(prompt("Enter the number"));
+//for(i=0; i<=a; a++)
+//{
+ //console.log("Number is"+" "+a);
+//}
+
+
+//var a = parseInt(prompt("Enter the number"));
+
+//for(i=0; i<=a; i++)
+//{
+ //console.log("Number is"+" "+a);
+//}
+
+
+
+//var n = parseInt(prompt("Enter the number"));
+//let i=0
+//do
+//{
+	//console.log(i);
+	//i++;
+//}
+//while(i <= n);
+
+
+//for (let i = 0; i < 5; i++) {
+   //console.log("The number is " +"i");
+//}
